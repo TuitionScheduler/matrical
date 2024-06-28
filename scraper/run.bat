9f321f4e8f6e4dd0ff30fe6d7bee73c3761b4ed4
Poggecci <alejandro.cruzado@upr.edu>
2024-06-28 14:25:33 -0400

    update bat script to scrape all semesters

--- scraper/run.bat
+++ scraper/run.bat
@@ -1,4 +1,9 @@
-poetry run main.py 2 2024
-poetry run main.py 1 2024
-poetry run main.py 4 2024
-poetry run main.py 5 2024
+poetry run python main.py 2 2024
+poetry run python main.py 1 2024
+poetry run python main.py 4 2024
+poetry run python main.py 5 2024
+
+poetry run python main.py 2 2023 
+poetry run python main.py 1 2023
+poetry run python main.py 4 2023
+poetry run python main.py 5 2023
