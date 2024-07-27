4c0ba3387e022f63963f536ab0050b32e1fb2c97
Poggecci <alejandro.cruzado@upr.edu>
2024-07-27 17:20:40 -0400

    Add useful course search methods to course service

--- data_entry/src/models/course_service.py
+++ data_entry/src/models/course_service.py
@@ -7,11 +7,35 @@ class CourseService:
         Session = sessionmaker(engine)
         self.session = Session()
 
-    def getCourse(self, code, term, year) -> Course | None:
+    def getCourse(self, code: str, term: str | None, year: int | None) -> Course | None:
         self.session.begin()
-        course = (
-            self.session.query(Course)
-            .filter_by(course_code=code, year=year, term=term)
-            .first()
-        )
+        if term is None and year is None:
+            self.session.query(Course).filter_by(course_code=code)
+            course = self.session.query(Course).filter_by(course_code=code).first()
+        elif term is None:
+            course = (
+                self.session.query(Course)
+                .filter_by(course_code=code, year=year)
+                .first()
+            )
+        elif year is None:
+            course = (
+                self.session.query(Course)
+                .filter_by(course_code=code, term=term)
+                .order_by(Course.year.desc())
+                .first()
+            )
+        else:
+            course = (
+                self.session.query(Course)
+                .filter_by(course_code=code, term=term, year=year)
+                .first()
+            )
+        self.session.close()
         return course
+
+    def getAllCourses(self, term, year) -> list[Course]:
+        self.session.begin()
+        courses = self.session.query(Course).filter_by(year=year, term=term).all()
+        self.session.close()
+        return courses
