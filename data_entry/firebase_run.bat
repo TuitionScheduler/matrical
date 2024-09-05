16be164858a8ef6875f1b031b943274805e98153
Poggecci <alejandro.cruzado@upr.edu>
2024-09-04 22:38:46 -0400

    Fix firebase_run script to use new input parameters for scraper

--- data_entry/firebase_run.bat
+++ data_entry/firebase_run.bat
@@ -1,11 +1,11 @@
-poetry run python src/scrapers/scrape_to_firebase.py 1 2024
-poetry run python src/scrapers/scrape_to_firebase.py 2 2024
-poetry run python src/scrapers/scrape_to_firebase.py 3 2024
-poetry run python src/scrapers/scrape_to_firebase.py 4 2024
-poetry run python src/scrapers/scrape_to_firebase.py 5 2024
+poetry run python src/scrapers/scrape_to_firebase.py Fall 2024 24
+poetry run python src/scrapers/scrape_to_firebase.py Spring 2024 24
+poetry run python src/scrapers/scrape_to_firebase.py FirstSummer 2024 10
+poetry run python src/scrapers/scrape_to_firebase.py SecondSummer 2024 5
+poetry run python src/scrapers/scrape_to_firebase.py ExtendedSummer 2024 5
 
-poetry run python src/scrapers/scrape_to_firebase.py 1 2023
-poetry run python src/scrapers/scrape_to_firebase.py 2 2023
-poetry run python src/scrapers/scrape_to_firebase.py 3 2023
-poetry run python src/scrapers/scrape_to_firebase.py 4 2023
-poetry run python src/scrapers/scrape_to_firebase.py 5 2023
+poetry run python src/scrapers/scrape_to_firebase.py Fall 2023 24
+poetry run python src/scrapers/scrape_to_firebase.py Spring 2023 24
+poetry run python src/scrapers/scrape_to_firebase.py FirstSummer 2023 10
+poetry run python src/scrapers/scrape_to_firebase.py SecondSummer 2023 5
+poetry run python src/scrapers/scrape_to_firebase.py ExtendedSummer 2023 5
